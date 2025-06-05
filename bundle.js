773da2e002405bb7441bc1a6093fe08b6b80ce41
simplylisaa <simply.lisaa1@gmail.com>
2025-06-05 14:51:25 +0200

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,233 @@
+"use strict";
+(() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+    mod
+  ));
+
+  // node_modules/bondage-club-mod-sdk/dist/bcmodsdk.js
+  var require_bcmodsdk = __commonJS({
+    "node_modules/bondage-club-mod-sdk/dist/bcmodsdk.js"(exports) {
+      var bcModSdk = function() {
+        "use strict";
+        const o = "1.2.0";
+        function e(o2) {
+          alert("Mod ERROR:\n" + o2);
+          const e2 = new Error(o2);
+          throw console.error(e2), e2;
+        }
+        const t = new TextEncoder();
+        function n(o2) {
+          return !!o2 && "object" == typeof o2 && !Array.isArray(o2);
+        }
+        function r(o2) {
+          const e2 = /* @__PURE__ */ new Set();
+          return o2.filter((o3) => !e2.has(o3) && e2.add(o3));
+        }
+        const i = /* @__PURE__ */ new Map(), a = /* @__PURE__ */ new Set();
+        function c(o2) {
+          a.has(o2) || (a.add(o2), console.warn(o2));
+        }
+        function s(o2) {
+          const e2 = [], t2 = /* @__PURE__ */ new Map(), n2 = /* @__PURE__ */ new Set();
+          for (const r3 of f.values()) {
+            const i3 = r3.patching.get(o2.name);
+            if (i3) {
+              e2.push(...i3.hooks);
+              for (const [e3, a2] of i3.patches.entries()) t2.has(e3) && t2.get(e3) !== a2 && c(`ModSDK: Mod '${r3.name}' is patching function ${o2.name} with same pattern that is already applied by different mod, but with different pattern:
+Pattern:
+${e3}
+Patch1:
+${t2.get(e3) || ""}
+Patch2:
+${a2}`), t2.set(e3, a2), n2.add(r3.name);
+            }
+          }
+          e2.sort((o3, e3) => e3.priority - o3.priority);
+          const r2 = function(o3, e3) {
+            if (0 === e3.size) return o3;
+            let t3 = o3.toString().replaceAll("\r\n", "\n");
+            for (const [n3, r3] of e3.entries()) t3.includes(n3) || c(`ModSDK: Patching ${o3.name}: Patch ${n3} not applied`), t3 = t3.replaceAll(n3, r3);
+            return (0, eval)(`(${t3})`);
+          }(o2.original, t2);
+          let i2 = function(e3) {
+            var t3, i3;
+            const a2 = null === (i3 = (t3 = m.errorReporterHooks).hookChainExit) || void 0 === i3 ? void 0 : i3.call(t3, o2.name, n2), c2 = r2.apply(this, e3);
+            return null == a2 || a2(), c2;
+          };
+          for (let t3 = e2.length - 1; t3 >= 0; t3--) {
+            const n3 = e2[t3], r3 = i2;
+            i2 = function(e3) {
+              var t4, i3;
+              const a2 = null === (i3 = (t4 = m.errorReporterHooks).hookEnter) || void 0 === i3 ? void 0 : i3.call(t4, o2.name, n3.mod), c2 = n3.hook.apply(this, [e3, (o3) => {
+                if (1 !== arguments.length || !Array.isArray(e3)) throw new Error(`Mod ${n3.mod} failed to call next hook: Expected args to be array, got ${typeof o3}`);
+                return r3.call(this, o3);
+              }]);
+              return null == a2 || a2(), c2;
+            };
+          }
+          return { hooks: e2, patches: t2, patchesSources: n2, enter: i2, final: r2 };
+        }
+        function l(o2, e2 = false) {
+          let r2 = i.get(o2);
+          if (r2) e2 && (r2.precomputed = s(r2));
+          else {
+            let e3 = window;
+            const a2 = o2.split(".");
+            for (let t2 = 0; t2 < a2.length - 1; t2++) if (e3 = e3[a2[t2]], !n(e3)) throw new Error(`ModSDK: Function ${o2} to be patched not found; ${a2.slice(0, t2 + 1).join(".")} is not object`);
+            const c2 = e3[a2[a2.length - 1]];
+            if ("function" != typeof c2) throw new Error(`ModSDK: Function ${o2} to be patched not found`);
+            const l2 = function(o3) {
+              let e4 = -1;
+              for (const n2 of t.encode(o3)) {
+                let o4 = 255 & (e4 ^ n2);
+                for (let e5 = 0; e5 < 8; e5++) o4 = 1 & o4 ? -306674912 ^ o4 >>> 1 : o4 >>> 1;
+                e4 = e4 >>> 8 ^ o4;
+              }
+              return ((-1 ^ e4) >>> 0).toString(16).padStart(8, "0").toUpperCase();
+            }(c2.toString().replaceAll("\r\n", "\n")), d2 = { name: o2, original: c2, originalHash: l2 };
+            r2 = Object.assign(Object.assign({}, d2), { precomputed: s(d2), router: () => {
+            }, context: e3, contextProperty: a2[a2.length - 1] }), r2.router = /* @__PURE__ */ function(o3) {
+              return function(...e4) {
+                return o3.precomputed.enter.apply(this, [e4]);
+              };
+            }(r2), i.set(o2, r2), e3[r2.contextProperty] = r2.router;
+          }
+          return r2;
+        }
+        function d() {
+          for (const o2 of i.values()) o2.precomputed = s(o2);
+        }
+        function p() {
+          const o2 = /* @__PURE__ */ new Map();
+          for (const [e2, t2] of i) o2.set(e2, { name: e2, original: t2.original, originalHash: t2.originalHash, sdkEntrypoint: t2.router, currentEntrypoint: t2.context[t2.contextProperty], hookedByMods: r(t2.precomputed.hooks.map((o3) => o3.mod)), patchedByMods: Array.from(t2.precomputed.patchesSources) });
+          return o2;
+        }
+        const f = /* @__PURE__ */ new Map();
+        function u(o2) {
+          f.get(o2.name) !== o2 && e(`Failed to unload mod '${o2.name}': Not registered`), f.delete(o2.name), o2.loaded = false, d();
+        }
+        function g(o2, t2) {
+          o2 && "object" == typeof o2 || e("Failed to register mod: Expected info object, got " + typeof o2), "string" == typeof o2.name && o2.name || e("Failed to register mod: Expected name to be non-empty string, got " + typeof o2.name);
+          let r2 = `'${o2.name}'`;
+          "string" == typeof o2.fullName && o2.fullName || e(`Failed to register mod ${r2}: Expected fullName to be non-empty string, got ${typeof o2.fullName}`), r2 = `'${o2.fullName} (${o2.name})'`, "string" != typeof o2.version && e(`Failed to register mod ${r2}: Expected version to be string, got ${typeof o2.version}`), o2.repository || (o2.repository = void 0), void 0 !== o2.repository && "string" != typeof o2.repository && e(`Failed to register mod ${r2}: Expected repository to be undefined or string, got ${typeof o2.version}`), null == t2 && (t2 = {}), t2 && "object" == typeof t2 || e(`Failed to register mod ${r2}: Expected options to be undefined or object, got ${typeof t2}`);
+          const i2 = true === t2.allowReplace, a2 = f.get(o2.name);
+          a2 && (a2.allowReplace && i2 || e(`Refusing to load mod ${r2}: it is already loaded and doesn't allow being replaced.
+Was the mod loaded multiple times?`), u(a2));
+          const c2 = (o3) => {
+            let e2 = g2.patching.get(o3.name);
+            return e2 || (e2 = { hooks: [], patches: /* @__PURE__ */ new Map() }, g2.patching.set(o3.name, e2)), e2;
+          }, s2 = (o3, t3) => (...n2) => {
+            var i3, a3;
+            const c3 = null === (a3 = (i3 = m.errorReporterHooks).apiEndpointEnter) || void 0 === a3 ? void 0 : a3.call(i3, o3, g2.name);
+            g2.loaded || e(`Mod ${r2} attempted to call SDK function after being unloaded`);
+            const s3 = t3(...n2);
+            return null == c3 || c3(), s3;
+          }, p2 = { unload: s2("unload", () => u(g2)), hookFunction: s2("hookFunction", (o3, t3, n2) => {
+            "string" == typeof o3 && o3 || e(`Mod ${r2} failed to patch a function: Expected function name string, got ${typeof o3}`);
+            const i3 = l(o3), a3 = c2(i3);
+            "number" != typeof t3 && e(`Mod ${r2} failed to hook function '${o3}': Expected priority number, got ${typeof t3}`), "function" != typeof n2 && e(`Mod ${r2} failed to hook function '${o3}': Expected hook function, got ${typeof n2}`);
+            const s3 = { mod: g2.name, priority: t3, hook: n2 };
+            return a3.hooks.push(s3), d(), () => {
+              const o4 = a3.hooks.indexOf(s3);
+              o4 >= 0 && (a3.hooks.splice(o4, 1), d());
+            };
+          }), patchFunction: s2("patchFunction", (o3, t3) => {
+            "string" == typeof o3 && o3 || e(`Mod ${r2} failed to patch a function: Expected function name string, got ${typeof o3}`);
+            const i3 = l(o3), a3 = c2(i3);
+            n(t3) || e(`Mod ${r2} failed to patch function '${o3}': Expected patches object, got ${typeof t3}`);
+            for (const [n2, i4] of Object.entries(t3)) "string" == typeof i4 ? a3.patches.set(n2, i4) : null === i4 ? a3.patches.delete(n2) : e(`Mod ${r2} failed to patch function '${o3}': Invalid format of patch '${n2}'`);
+            d();
+          }), removePatches: s2("removePatches", (o3) => {
+            "string" == typeof o3 && o3 || e(`Mod ${r2} failed to patch a function: Expected function name string, got ${typeof o3}`);
+            const t3 = l(o3);
+            c2(t3).patches.clear(), d();
+          }), callOriginal: s2("callOriginal", (o3, t3, n2) => {
+            "string" == typeof o3 && o3 || e(`Mod ${r2} failed to call a function: Expected function name string, got ${typeof o3}`);
+            const i3 = l(o3);
+            return Array.isArray(t3) || e(`Mod ${r2} failed to call a function: Expected args array, got ${typeof t3}`), i3.original.apply(null != n2 ? n2 : globalThis, t3);
+          }), getOriginalHash: s2("getOriginalHash", (o3) => {
+            "string" == typeof o3 && o3 || e(`Mod ${r2} failed to get hash: Expected function name string, got ${typeof o3}`);
+            return l(o3).originalHash;
+          }) }, g2 = { name: o2.name, fullName: o2.fullName, version: o2.version, repository: o2.repository, allowReplace: i2, api: p2, loaded: true, patching: /* @__PURE__ */ new Map() };
+          return f.set(o2.name, g2), Object.freeze(p2);
+        }
+        function h() {
+          const o2 = [];
+          for (const e2 of f.values()) o2.push({ name: e2.name, fullName: e2.fullName, version: e2.version, repository: e2.repository });
+          return o2;
+        }
+        let m;
+        const y = void 0 === window.bcModSdk ? window.bcModSdk = function() {
+          const e2 = { version: o, apiVersion: 1, registerMod: g, getModsInfo: h, getPatchingInfo: p, errorReporterHooks: Object.seal({ apiEndpointEnter: null, hookEnter: null, hookChainExit: null }) };
+          return m = e2, Object.freeze(e2);
+        }() : (n(window.bcModSdk) || e("Failed to init Mod SDK: Name already in use"), 1 !== window.bcModSdk.apiVersion && e(`Failed to init Mod SDK: Different version already loaded ('1.2.0' vs '${window.bcModSdk.version}')`), window.bcModSdk.version !== o && alert(`Mod SDK warning: Loading different but compatible versions ('1.2.0' vs '${window.bcModSdk.version}')
+One of mods you are using is using an old version of SDK. It will work for now but please inform author to update`), window.bcModSdk);
+        return "undefined" != typeof exports && (Object.defineProperty(exports, "__esModule", { value: true }), exports.default = y), y;
+      }();
+    }
+  });
+
+  // src/index.ts
+  var import_bondage_club_mod_sdk = __toESM(require_bcmodsdk());
+  var modApi = import_bondage_club_mod_sdk.default.registerMod({
+    name: "BetterRemotes",
+    fullName: "Better Remotes by Lisa",
+    version: "0.2"
+  });
+  modApi.hookFunction("DialogCanUseRemoteState", 0, (args, next) => {
+    const [C, Item] = args;
+    if (C.MemberNumber !== Player.MemberNumber) {
+      return next(args);
+    }
+    if (!Item || !InventoryItemHasEffect(Item, "UseRemote")) return "InvalidItem";
+    if (!Player.CanInteract()) {
+      if (InventoryGet(Player, "ItemHandheld")?.Asset.Name === "VibeRemote") {
+        return "Available";
+      }
+      return "CannotInteract";
+    }
+    if (C.IsPlayer() && Player.IsFullyOwned() && LogQuery("BlockRemoteSelf", "OwnerRule")) return "RemotesBlocked";
+    if (Item.Asset.LoverOnly) {
+      if (!C.IsLoverOfPlayer()) {
+        return "NoAccess";
+      } else if (!InventoryAvailable(Player, "LoversVibratorRemote", "ItemVulva")) {
+        return "NoLoversRemote";
+      } else {
+        return "Available";
+      }
+    } else {
+      if (C.Effect.indexOf("BlockRemotes") >= 0) {
+        return "RemotesBlocked";
+      }
+      if (!InventoryAvailable(Player, "VibratorRemote", "ItemVulva")) {
+        return "NoRemote";
+      }
+      if (LogQuery("BlockRemote", "OwnerRule")) {
+        return "NoRemoteOwnerRuleActive";
+      }
+      return "Available";
+    }
+  });
+})();
